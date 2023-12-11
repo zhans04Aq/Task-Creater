bb413a81074d1bc6242b37fa1fabb02ef24e6aca
zhans04Aq <zhans.bazarov@gmail.com>
2023-12-11 15:39:13 +0600

    priority functionality

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const popUp = document.querySelector(".pop-up-window")
 
 const closePopUp = document.querySelector('.close-pop-up')
 
+const highPrior = document.querySelector('#high')
+const midPrior = document.querySelector('#mid')
+const lowPrior = document.querySelector('#low')
+const currentPrior = document.querySelector('.current-priority')
+
+const createButton = document.querySelector('.create-task-btn')
+
 addTaskBtn.addEventListener('click', ()=>{
     popUp.classList.add('active')
 })
@@ -11,3 +18,24 @@ addTaskBtn.addEventListener('click', ()=>{
 closePopUp.addEventListener('click', ()=>{
     popUp.classList.remove('active')
 })
+
+highPrior.addEventListener('click', ()=>{
+    currentPrior.removeAttribute('id');
+    currentPrior.setAttribute('id', 'high')
+})
+
+midPrior.addEventListener('click', ()=>{
+    currentPrior.removeAttribute('id');
+    currentPrior.setAttribute('id', 'mid')
+})
+
+
+lowPrior.addEventListener('click', ()=>{
+    currentPrior.removeAttribute('id');
+    currentPrior.setAttribute('id', 'low')
+})
+
+
+// document.querySelector('div').removeAttribute('id');
+
+// myPara.setAttribute("id", "id_you_like");
