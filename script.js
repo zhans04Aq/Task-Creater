abd413ccea089aaccf346898cfeaa1e354794d44
zhans04Aq <zhans.bazarov@gmail.com>
2023-12-11 15:16:46 +0600

    added addTaskBtn functionality and some css

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const addTaskBtn = document.querySelector(".addTaskBtn")
 
 const popUp = document.querySelector(".pop-up-window")
 
+const closePopUp = document.querySelector('.close-pop-up')
+
+addTaskBtn.addEventListener('click', ()=>{
+    popUp.classList.add('active')
+})
+
+closePopUp.addEventListener('click', ()=>{
+    popUp.classList.remove('active')
+})
