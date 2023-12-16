54a78e780ba7379aaa48d57e7f69f658397a51f4
zhans04Aq <145835709+zhans04Aq@users.noreply.github.com>
2023-12-16 15:44:55 +0500

    styled popup and postioned add task btn(also styled)

--- script.js
+++ script.js
@@ -41,70 +41,10 @@ lowPrior.addEventListener('click', ()=>{
     currentPrior.setAttribute('id', 'low')
 })
 
-createButton.addEventListener('click', ()=>{
-    let colName = document.querySelector('.column-name')
-    let colVal = colName.value
-    let currPriorVal = ''
-    if(currentPrior.getAttribute("id")=='high'){
-        currPriorVal = 'high'
-        popUp.classList.remove('active') 
-    } else if (currentPrior.getAttribute('id')=='mid'){
-        currPriorVal ='mid'
-        popUp.classList.remove('active') 
-    } else if(currentPrior.getAttribute('id')=='low'){
-        currPriorVal = 'low'
-        popUp.classList.remove('active') 
-    } else{
-        alert('u did not set priority')
-    }
-    const taskDiv = document.createElement('div')   
-    taskDiv.classList.add('task')
-    const taskPriority = document.createElement('div')
-    taskPriority.classList.add('task-priority')
-    document.querySelector('.task-wrapper').append(taskDiv)
-
-
-    const taskInput = document.getElementById('taskInput');
-    const taskName = document.getElementById('taskName');
-
-    taskNameHeading.textContent = taskInput.value;
-
-    taskDiv.append('')
-})
 
 
 
 
-document.querySelector('.add-todo-task').addEventListener('click', function () {
-    const todoWrapper = document.querySelector('.todo-wrapper');
-    
-
-    const todo = document.createElement('div');
-    todo.classList.add('todo');
-
-
-    const todoInp = document.createElement('input');
-    todoInp.classList.add('todo-input');
-
-
-    const deleteTodoBtn = document.createElement('button');
-    deleteTodoBtn.classList.add('delete-todo');
-    deleteTodoBtn.textContent = '-';
-    
-
-    deleteTodoBtn.addEventListener('click', function () {
-        todo.remove();
-        adjustTaskHeight();
-    });
-
-    todo.appendChild(todoInp);
-    todo.appendChild(deleteTodoBtn);
-
-
-    todoWrapper.appendChild(todo);
-
-
-});
 
 
 
